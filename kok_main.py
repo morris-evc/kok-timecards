4c1638643c3ef1b4aeb29703949ff10f1ee213ad
Evan Morris <emorris@muc.muc-corp.com>
2021-07-30 14:54:54 -0400

    Added all list files with .pdf extesion to list. loop reads all file paths in list and adds them to dataframe .to_csv added

--- kok_main.py
+++ kok_main.py
@@ -1,31 +1,54 @@
 import pandas as pd
 import PyPDF2
 import pprint
+import glob
 import os
 pp = pprint.PrettyPrinter()
-pdf_file = open(r'c:\users\emorris\desktop\git\kok-timecards\timecard_pdfs\6 26 21 Time Card.pdf', 'rb')
 
-reader = PyPDF2.PdfFileReader(pdf_file)
-page = reader.getPage(0)
-#print(page.extractText())
-field_data_list = []
+
+# looping through .pdf extensions only
+pdf_list = []
+os.chdir("timecard_pdfs")
+for file in glob.glob("*.pdf"):
+    pdf_list.append(file)
+
+pp.pprint(pdf_list)
+
 col_names = ["Employee Name", "Employee Number", "Total Hrs", "Total OT Hrs", "Regularhrs1", "Regularhrs2", "Regularhrs3",
-"Regularhrs4", "Regularhrs5", "Regularhrs6", "Regularhrs7"]
+    "Regularhrs4", "Regularhrs5", "Regularhrs6", "Regularhrs7", "Overtimehrs1", "Overtimehrs2", "Overtimehrs3",
+    "Overtimehrs4", "Overtimehrs5", "Overtimehrs6", "Overtimehrs7", "Super1", "Super2", "Super3", "Super4", "Super5",
+    "Super6", "Super7"]
 
 df = pd.DataFrame(columns=col_names)
 
+row_num = 0
 
-page_fields_list = [page.extractText()]
-#pp.pprint(page_fields_list)
-#field_data_list = [reader.getFields()]
-fields = reader.getFields()
+for f in pdf_list:
 
+    #pdf_file = open(r'c:\users\emorris\desktop\git\kok-timecards\timecard_pdfs\6 26 21 Time Card.pdf', 'rb')
+    pdf_file = open(f, 'rb')
+    reader = PyPDF2.PdfFileReader(pdf_file)
+    page = reader.getPage(0)
+    #print(page.extractText())
+    field_data_list = []
+    
+    row_num += 1
 
-for x in col_names:
-    df.loc[1, x] = fields[x].value
-    #print(fields[x].value)
+    page_fields_list = [page.extractText()]
+    #pp.pprint(page_fields_list)
+    #field_data_list = [reader.getFields()]
+    fields = reader.getFields()
+   
+
+    for x in col_names:
+        
+        df.loc[row_num, x] = fields[x].value
+        #print(fields[x].value)
+    
 print(df)
-
+df.to_csv('test.csv')
+#testing csv
+#df.to_csv('test.csv')
 #pp.pprint(field_data_list)
 #print(field_data_list.count)
 
